1b7b9aed09918e9c99d3634b85729fa23f1c5138
Dena Meidina <dena@gits.id>
2022-11-08 10:29:10 +0700

    feat(widget): add visibility properties in device hightlight

--- lib/widgets/device_highlight.dart
+++ lib/widgets/device_highlight.dart
@@ -27,6 +27,7 @@ class DeviceHighlight extends StatefulWidget {
 class _DeviceHighlightState extends State<DeviceHighlight>
     with AutomaticKeepAliveClientMixin<DeviceHighlight> {
   bool isOpen = false;
+  bool isOpenProperties = true;
 
   @override
   bool get wantKeepAlive => true;
@@ -50,7 +51,7 @@ class _DeviceHighlightState extends State<DeviceHighlight>
         SizedBox(
           height: MediaQuery.of(context).size.height * 0.8,
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Expanded(
                 child: DeviceFrame(
@@ -71,93 +72,134 @@ class _DeviceHighlightState extends State<DeviceHighlight>
               const SizedBox(width: 16),
               const VerticalDivider(thickness: 1, width: 1),
               const SizedBox(width: 16),
-              Container(
-                constraints: const BoxConstraints(
-                  maxWidth: 300,
-                ),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
+              if (!isOpenProperties)
+                Column(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isOpenProperties = true;
+                        });
+                      },
+                      icon: const Icon(Icons.keyboard_arrow_left),
+                    ),
+                    RotatedBox(
+                      quarterTurns: 1,
+                      child: Text(
                         'Properties',
                         style: context.titleMedium?.copyWith(
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      const SizedBox(height: 8),
-                      CardProperties(
-                        title: 'Theme Mode',
-                        selected: themeModeSimulation.name,
-                        items: [
-                          ThemeMode.light.name,
-                          ThemeMode.dark.name,
-                        ],
-                        onChanged: (value) {
-                          context.read<GlobalCubit>().setThemeModeSimulation(
-                                ThemeMode.values.firstWhereOrNull(
-                                      (element) => element.name == value,
-                                    ) ??
-                                    ThemeMode.light,
-                              );
-                        },
-                      ),
-                      const SizedBox(height: 8),
-                      CardProperties(
-                        title: 'Orientation',
-                        selected: orientationSimulation.name,
-                        items: [
-                          Orientation.portrait.name,
-                          Orientation.landscape.name,
-                        ],
-                        onChanged: (value) {
-                          context.read<GlobalCubit>().setOrientationSimulation(
-                                Orientation.values.firstWhereOrNull(
-                                      (element) => element.name == value,
-                                    ) ??
-                                    Orientation.portrait,
-                              );
-                        },
-                      ),
-                      const SizedBox(height: 8),
-                      CardProperties(
-                        title: 'Frames',
-                        selected:
-                            isFrameVisibleSimulation ? 'Device Frame' : 'None',
-                        items: const [
-                          'Device Frame',
-                          'None',
-                        ],
-                        onChanged: (value) {
-                          context.read<GlobalCubit>().setFrameVisibleSimulation(
-                                value == 'Device Frame',
-                              );
-                        },
-                      ),
-                      const SizedBox(height: 8),
-                      CardProperties(
-                        title: 'Devices',
-                        selected: deviceInfoSimulation.name,
-                        items: [
-                          ...Devices.ios.all.map((e) => e.name),
-                        ],
-                        onChanged: (value) {
-                          final ios = Devices.ios.all.firstWhereOrNull(
-                              (element) => element.name == value);
-
-                          context.read<GlobalCubit>().setDeviceInfoSimulation(
-                                ios ?? Devices.ios.iPhone13,
-                              );
-                        },
-                      ),
-                      if (widget.properties?.isNotEmpty ?? false) ...[
+                    ),
+                  ],
+                )
+              else
+                Container(
+                  constraints: const BoxConstraints(
+                    maxWidth: 300,
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  isOpenProperties = false;
+                                });
+                              },
+                              icon: const Icon(Icons.keyboard_arrow_right),
+                            ),
+                            const SizedBox(width: 8),
+                            Text(
+                              'Properties',
+                              style: context.titleMedium?.copyWith(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
                         const SizedBox(height: 8),
-                        ...widget.properties!,
+                        CardProperties(
+                          title: 'Theme Mode',
+                          selected: themeModeSimulation.name,
+                          items: [
+                            ThemeMode.light.name,
+                            ThemeMode.dark.name,
+                          ],
+                          onChanged: (value) {
+                            context.read<GlobalCubit>().setThemeModeSimulation(
+                                  ThemeMode.values.firstWhereOrNull(
+                                        (element) => element.name == value,
+                                      ) ??
+                                      ThemeMode.light,
+                                );
+                          },
+                        ),
+                        const SizedBox(height: 8),
+                        CardProperties(
+                          title: 'Orientation',
+                          selected: orientationSimulation.name,
+                          items: [
+                            Orientation.portrait.name,
+                            Orientation.landscape.name,
+                          ],
+                          onChanged: (value) {
+                            context
+                                .read<GlobalCubit>()
+                                .setOrientationSimulation(
+                                  Orientation.values.firstWhereOrNull(
+                                        (element) => element.name == value,
+                                      ) ??
+                                      Orientation.portrait,
+                                );
+                          },
+                        ),
+                        const SizedBox(height: 8),
+                        CardProperties(
+                          title: 'Frames',
+                          selected: isFrameVisibleSimulation
+                              ? 'Device Frame'
+                              : 'None',
+                          items: const [
+                            'Device Frame',
+                            'None',
+                          ],
+                          onChanged: (value) {
+                            context
+                                .read<GlobalCubit>()
+                                .setFrameVisibleSimulation(
+                                  value == 'Device Frame',
+                                );
+                          },
+                        ),
+                        const SizedBox(height: 8),
+                        CardProperties(
+                          title: 'Devices',
+                          selected: deviceInfoSimulation.name,
+                          items: [
+                            ...Devices.ios.all.map((e) => e.name),
+                          ],
+                          onChanged: (value) {
+                            final ios = Devices.ios.all.firstWhereOrNull(
+                                (element) => element.name == value);
+
+                            context.read<GlobalCubit>().setDeviceInfoSimulation(
+                                  ios ?? Devices.ios.iPhone13,
+                                );
+                          },
+                        ),
+                        if (widget.properties?.isNotEmpty ?? false) ...[
+                          const SizedBox(height: 8),
+                          ...widget.properties!,
+                        ],
                       ],
-                    ],
+                    ),
                   ),
                 ),
-              ),
             ],
           ),
         ),
