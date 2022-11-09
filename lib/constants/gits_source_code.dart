9f147f7e471c320865b375deff15b85651995b76
Dena Meidina <dena@gits.id>
2022-11-10 06:59:38 +0700

    refactor(spacing): move code snippet from global to spesific file

--- lib/constants/gits_source_code.dart
+++ lib/constants/gits_source_code.dart
@@ -3,45 +3,6 @@ abstract class GitsSourceCode {
           appBar: AppBar(title: const Text("Gits")),
           body: const GitsEmptyState(text: "Belum ada data yang diinputkan"),
         ),''';
-  static const String codeSnippetSpacing = '''Scaffold(
-          appBar: AppBar(),
-          body: Padding(
-            padding: const EdgeInsets.all(GitsSizes.s16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: GitsSizes.s16),
-                  child: Text(
-                    "Gits Spacing Vertical & Horizontal",
-                    style: TextStyle(
-                      fontSize: GitsSizes.s20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const Text("Gits spacing vertical size 16"),
-                const GitsSpacing.vertical16(),
-                const Text("Gits spacing vertical when set manual value 30"),
-                const GitsSpacing.vertical(size: 30),
-                Row(
-                  children: const [
-                    Text("Gits spacing"),
-                    GitsSpacing.horizontal16(),
-                    Text("horizontal size 16"),
-                  ],
-                ),
-                Row(
-                  children: const [
-                    Text("Gits spacing horizontal"),
-                    GitsSpacing.horizontal(size: 8),
-                    Text("when set manual size 8"),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),''';
   static const String codeSnippetShimmer = '''GitsShimmer(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
