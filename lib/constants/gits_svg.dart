933dcaac4b5b205fb8f05fd735d0570fb1ff796f
yusupdireja <yusupdireja@gmail.com>
2022-11-17 17:06:20 +0700

    feat(assets) : add password illustration

--- lib/constants/gits_svg.dart
+++ lib/constants/gits_svg.dart
@@ -1,3 +1,5 @@
 abstract class GitsSvg {
   static const String emptyIllustration = 'assets/svg/empty-illustration.svg';
+  static const String passwordIllustration =
+      'assets/svg/password-illustration.svg';
 }
