6611a04210b942314f098afcd2a2467e2694817c
yusupdireja <yusupdireja@gmail.com>
2022-11-24 18:55:18 +0700

    feat(extension) : add date time extension

---
+++ lib/pages/extension/date_time_extension.dart
@@ -0,0 +1,10 @@
+import 'package:intl/intl.dart';
+
+extension DateTimeExtension on DateTime {
+  String toFullDateTime() =>
+      DateFormat('EEEE, dd MMM yyyy - HH:mm', 'id').format(this);
+  String toSimpleDateTime() =>
+      DateFormat('dd MMM yyyy, HH:mm', 'id').format(this);
+  String toFormatDate([String? pattern, String? locale]) =>
+      DateFormat(pattern ?? 'dd MMM yyyy', locale ?? 'id').format(this);
+}
