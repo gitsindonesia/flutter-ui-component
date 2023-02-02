73430bdc8b5e6afb6d92e9d8b84876d11351d569
yusupdireja <yusupdireja@gmail.com>
2023-02-03 00:16:16 +0700

    fix(page) : adjust page gits player page

--- lib/pages/component/gits_player/gits_player_page.dart
+++ lib/pages/component/gits_player/gits_player_page.dart
@@ -12,7 +12,7 @@ class GitsPlayerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScaffoldPage(
-      appBar: AppBar(title: const Text('Gits Search')),
+      appBar: AppBar(title: const Text('Gits Player')),
       children: [
         CardHighlight(
           codeSnippet: codeSnippetGitsPlayer,
@@ -41,7 +41,7 @@ class _ExampleGitsPlayerState extends State<ExampleGitsPlayer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Example Gits Search')),
+      appBar: AppBar(title: const Text('Example Gits Player')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
